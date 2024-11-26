9ec48e1c3f71dc1dd0154203c029354cdfc45070
Joice Bergamin <joicenafef@gmail.com>
2024-11-26 11:11:55 -0300

    Commit 5: Criação das Repositories

---
+++ src/main/java/com/desenvolvimento/repositories/InstrutorRepository.java
@@ -0,0 +1,9 @@
+package com.desenvolvimento.repositories;
+
+import com.desenvolvimento.domains.Instrutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstrutorRepository extends JpaRepository<Instrutor, Integer> {
+}
