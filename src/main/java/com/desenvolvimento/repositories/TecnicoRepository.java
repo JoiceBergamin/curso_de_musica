85f60bface6c0751c674f1a755fbd1728a262d91
Joice Bergamin <joicenafef@gmail.com>
2025-03-24 15:04:40 -0300

    Commit 24: Criação das Repositories de Pessoa, Usuario, Tecnico e OrdemDeMatricula

---
+++ src/main/java/com/desenvolvimento/repositories/TecnicoRepository.java
@@ -0,0 +1,9 @@
+package com.desenvolvimento.repositories;
+
+import com.desenvolvimento.domains.Tecnico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
+}
