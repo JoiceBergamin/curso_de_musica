cb7f870870abbd08118c8191430a0a72622c00ef
Joice Bergamin <joicenafef@gmail.com>
2025-03-24 13:43:17 -0300

    Commit 21: Criação da classe abstrata Pessoa

---
+++ src/main/java/com/desenvolvimento/domains/Pessoa.java
@@ -0,0 +1,112 @@
+package com.desenvolvimento.domains;
+
+import com.desenvolvimento.domains.enums.TipoPessoa;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public abstract class Pessoa {
+
+    protected Long id;
+    protected String nome;
+    protected String sobrenome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+    protected LocalDate criadoEm = LocalDate.now();
+    protected Set<Integer> tipoPessoa = new HashSet<>();
+
+    public Pessoa() {
+        addTipoPessoa(TipoPessoa.USUARIO);
+    }
+
+    public Pessoa(Long id, String nome, String sobrenome, String cpf, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        addTipoPessoa(TipoPessoa.USUARIO);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDate criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public Set<TipoPessoa> getTipoPessoa() {
+        return tipoPessoa.stream().map(TipoPessoa::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addTipoPessoa(TipoPessoa tipoPessoa) {
+        this.tipoPessoa.add(tipoPessoa.getId());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id) && Objects.equals(cpf, pessoa.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
+}
